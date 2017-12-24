3b3819d950106cc19a9cd581e0793ba82db61caa
Ob Rzwo <obr2pd@gmail.com>
2017-12-24 22:58:06 +0000

    LED einzeln ansteuerbar.

--- LED.py
+++ LED.py
@@ -1,20 +1,22 @@
+import argparse
 import random
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(14,GPIO.OUT)
-#GPIO.setup(18,GPIO.OUT)
-try:
-    while True:
-        #print("LED on")
-        GPIO.output(14,GPIO.HIGH)
-        #GPIO.output(18,GPIO.HIGH)
-        time.sleep(random.random()*0.4)
-        #print("LED off")
-        GPIO.output(14,GPIO.LOW)
-        #GPIO.output(18,GPIO.LOW)
-        time.sleep(random.random()*0.4)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+def main(k):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(k, GPIO.OUT)
+    try:
+        while True:
+            GPIO.output(k, GPIO.HIGH)
+            time.sleep(random.random()*0.4)
+            GPIO.output(k, GPIO.LOW)
+            time.sleep(random.random()*0.4)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('k', type=int, help='GPIO number')
+args = parser.parse_args()
+main(args.k)
