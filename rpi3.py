6b047a42615f699606f0742e4530b07f01f6c57a
Ob Rzwo <obr2pd@gmail.com>
2017-12-28 18:36:58 +0000

    Spalte Gesamt-Bild fuer zw1 und zw2 auf.

--- rpi3.py
+++ rpi3.py
@@ -7,17 +7,26 @@ import zmq
 def main():
     # a = np.zeros([8,3], dtype=np.int32)
     # pins = np.where(a==0, GPIO.LOW, GPIO.HIGH)
+    pins = np.random.choice((GPIO.LOW, GPIO.HIGH), [8,18])
+    print(pins)
     context = zmq.Context()
-    socket = context.socket(zmq.REQ)  # Client
-    socket.connect('tcp://zw1:26231')
+    socket1 = context.socket(zmq.REQ)  # Client
+    socket2 = context.socket(zmq.REQ)  # Client
+    socket1.connect('tcp://zw1:26231')
+    socket2.connect('tcp://zw2:26231')
     while True:
-        pins = np.random.choice((GPIO.LOW, GPIO.HIGH), [8,3])
-        print(pins)
-        bmsg = pins.tostring()
-        socket.send(bmsg)
-        antwort = socket.recv()
-        print(antwort)
-        time.sleep(10)
+        pins = np.roll(pins, 1, axis=0)
+        pins1 = pins[:, 0:3]
+        pins2 = pins[:, 3:6]
+        bmsg1 = pins1.tostring()
+        bmsg2 = pins2.tostring()
+        socket1.send(bmsg1)
+        socket2.send(bmsg2)
+        antwort1 = socket1.recv()
+        #print(antwort1)
+        antwort2 = socket2.recv()
+        #print(antwort2)
+        time.sleep(1.0/10.0)
 
 if __name__ == "__main__":
     main()
