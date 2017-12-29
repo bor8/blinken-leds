b3ac194e1aa6b9b7a586902a9c13e01105c18051
Ob Rzwo <obr2pd@gmail.com>
2017-12-29 15:13:08 +0000

    Einbauen von Queue auf Zentral-RPi3.

--- rpi3.py
+++ rpi3.py
@@ -1,10 +1,18 @@
 import numpy as np
+import queue
 import RPi.GPIO as GPIO
 # import sys
 import time
 import zmq
 
 def main():
+    init_warten = 1.0
+    fps = 10.0
+    fpsk_min = 1.0 / 500.0
+    q_target = 10
+
+    fpsk = 1.0 / fps
+    q = queue.Queue()
     # a = np.zeros([8,3], dtype=np.int32)
     # pins = np.where(a==0, GPIO.LOW, GPIO.HIGH)
     pins = np.random.choice((GPIO.LOW, GPIO.HIGH), [8,18])
@@ -14,19 +22,40 @@ def main():
     socket2 = context.socket(zmq.REQ)  # Client
     socket1.connect('tcp://zw1:26231')
     socket2.connect('tcp://zw2:26231')
+    video_start_zeit = time.time() + init_warten
+    abspiel_soll_zeit_max = video_start_zeit
+    qsize1 = 0
     while True:
-        pins = np.roll(pins, 1, axis=0)
-        pins1 = pins[:, 0:3]
-        pins2 = pins[:, 3:6]
-        bmsg1 = pins1.tostring()
-        bmsg2 = pins2.tostring()
-        socket1.send(bmsg1)
-        socket2.send(bmsg2)
-        antwort1 = socket1.recv()
-        #print(antwort1)
-        antwort2 = socket2.recv()
-        #print(antwort2)
-        time.sleep(1.0/10.0)
+        while q.qsize() < q_target:
+            if abspiel_soll_zeit_max <= time.time():
+                print('Laggen!')
+                abspiel_soll_zeit_max = time.time() + init_warten
+            pins1 = pins[:, 0:3]
+            pins2 = pins[:, 3:6]
+            pins3 = pins[:, 6:9]
+            pins4 = pins[:, 9:12]
+            pins5 = pins[:, 12:15]
+            pins6 = pins[:, 15:18]
+            b1 = pins1.tostring()
+            b2 = pins2.tostring()
+            b3 = pins3.tostring()
+            b4 = pins4.tostring()
+            b5 = pins5.tostring()
+            b6 = pins6.tostring()
+            q.put((abspiel_soll_zeit_max, b1, b2, b3, b4, b5, b6))
+            abspiel_soll_zeit_max += fpsk
+            # print(pins)
+            pins = np.roll(pins, 1, axis=0)
+        if qsize1 < q_target:
+            while q.qsize() > 0:
+                ts, b1, b2, b3, b4, b5, b6 = q.get()
+                # print(ts)
+                socket1.send_pyobj((ts, b1))
+                qsize1 = socket1.recv_pyobj()
+        socket1.send_pyobj('Weiter!')
+        qsize1 = socket1.recv_pyobj()
+        # print(qsize1)
+        time.sleep(fpsk_min * 3.0)
 
 if __name__ == "__main__":
     main()
