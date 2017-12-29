13d85a80f406361c0f79ecd3edc13f4a858404ba
Ob Rzwo <obr2pd@gmail.com>
2017-12-29 22:35:44 +0000

    Bugfixe Diff.

--- rpi3.py
+++ rpi3.py
@@ -6,17 +6,18 @@ import time
 import zmq
 
 def main():
-    init_warten = 1.0
-    fps = 24.0
+    init_warten = 3.0
+    fps = 10.0
     fpsk_min = 1.0 / 500.0
     q_target = 100
 
     fpsk = 1.0 / fps
     q = queue.Queue()
-    # a = np.zeros([8,3], dtype=np.int32)
-    # pins = np.where(a==0, GPIO.LOW, GPIO.HIGH)
+    pins_alt = np.zeros([8,18], dtype=np.int32)
+    # pins = np.where(pins_alt==0, GPIO.LOW, GPIO.HIGH)
     pins = np.random.choice((GPIO.LOW, GPIO.HIGH), [8,18])
     print(pins)
+    pins_diff = pins - pins_alt
     context = zmq.Context()
     socket1 = context.socket(zmq.REQ)  # Client
     socket2 = context.socket(zmq.REQ)  # Client
@@ -30,12 +31,12 @@ def main():
             if abspiel_soll_zeit_max <= time.time():
                 print('Laggen!')
                 abspiel_soll_zeit_max = time.time() + init_warten
-            pins1 = pins[:, 0:3]
-            pins2 = pins[:, 3:6]
-            pins3 = pins[:, 6:9]
-            pins4 = pins[:, 9:12]
-            pins5 = pins[:, 12:15]
-            pins6 = pins[:, 15:18]
+            pins1 = pins_diff[:, 0:3]
+            pins2 = pins_diff[:, 3:6]
+            pins3 = pins_diff[:, 6:9]
+            pins4 = pins_diff[:, 9:12]
+            pins5 = pins_diff[:, 12:15]
+            pins6 = pins_diff[:, 15:18]
             b1 = pins1.tostring()
             b2 = pins2.tostring()
             b3 = pins3.tostring()
@@ -44,9 +45,9 @@ def main():
             b6 = pins6.tostring()
             q.put((abspiel_soll_zeit_max, b1, b2, b3, b4, b5, b6))
             abspiel_soll_zeit_max += fpsk
-            pins_alt = pins
             pins = np.roll(pins, 1, axis=1)
-            pins = pins - pins_alt
+            pins_diff = pins - pins_alt
+            pins_alt = np.copy(pins)
         if qsize1 < q_target:
             while q.qsize() > 0:
                 ts, b1, b2, b3, b4, b5, b6 = q.get()
