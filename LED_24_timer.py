c9614ddcbf3b2b2b6595722c6dc9b4af7009ff16
Ob Rzwo <obr2pd@gmail.com>
2019-10-22 01:42:03 +0200

    Adde Sekunden-Timer mit 24 LEDs.

---
+++ LED_24_timer.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+def main():
+    pins = list(range(1, 25))
+    GPIO.setmode(GPIO.BCM)
+    for k in pins:
+        GPIO.setup(k, GPIO.OUT)
+    zeit = int(time.time_ns() / (10 ** 9))  # in Sekunden
+    try:
+        while True:
+            zeit_vorher = zeit
+            zeit = int(time.time_ns() / (10 ** 9))  # in Sekunden
+            if zeit_vorher != zeit:
+                for k in pins:
+                    GPIO.output(k, GPIO.HIGH)
+                time.sleep(0.1)
+                for k in pins:
+                    GPIO.output(k, GPIO.LOW)
+            time.sleep(0.8)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+main()
