218aba28eb4a07f10b18b6f4a67225855646d2e6
Ob Rzwo <obr2pd@gmail.com>
2020-11-04 12:27:02 +0100

    More cronjob fixes.

--- cronjob.sh
+++ cronjob.sh
@@ -4,6 +4,8 @@ exec &> /home/pi/git/blinken-leds/cronjob.log
 
 echo 'Start: '$(date)
 
+[ "$1" = '--wait-for-network' ] && sleep 60
+
 # /usr/bin/screen -d -m
 
 # /usr/bin/python3 /home/pi/git/blinken-leds/LED_24_rider.py 0.1
@@ -11,6 +13,8 @@ echo 'Start: '$(date)
 
 # Slaves
 /usr/bin/python3 /home/pi/git/blinken-leds/zw.py
+
+[ "$1" = '--wait-for-network' ] && sleep 60
 
 # Master
 [ "$(hostname)" = 'zero1' ] && /usr/bin/python3 /home/pi/git/blinken-leds/rpi3.py
